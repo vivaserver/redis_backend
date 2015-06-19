1fcd21ea5d3a75ff159949e2f7ae6251c0d3eee8
Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
2015-06-19 09:37:58 -0300

    little, more meaningful spec

--- spec/redis_backend_spec.rb
+++ spec/redis_backend_spec.rb
@@ -4,4 +4,14 @@ describe RedisBackend do
   it 'exists' do
     assert RedisBackend
   end
+
+  if ENV['REDIS_SERVER']
+    it 'is available' do
+      assert $redis
+    end
+  else
+    it 'is not available' do
+      refute $redis
+    end
+  end
 end
